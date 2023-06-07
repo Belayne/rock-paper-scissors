7cbed3b94f44a2b37d1cddaff57e110037516915
Naol <37399395+Belayne@users.noreply.github.com>
2023-06-07 18:07:28 +0200

    Add user choice

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,15 @@
 
 const MOVES = ["ROCK", "PAPER", "SCISSORS"];
+const inputMessage = "Rock! Paper! Scissors!\nType your choice"
 
 function getComputerChoice() {
-    let computerMove = MOVES[Math.floor(Math.random() * 3)];
-    return computerMove;
+    let computerChoice = MOVES[Math.floor(Math.random() * 3)];
+    return computerChoice;
 }
+
+function getUserChoice() {
+    let userChoice = prompt(inputMessage).toUpperCase();
+    return userChoice;
+}
+
+console.log(getUserChoice());
