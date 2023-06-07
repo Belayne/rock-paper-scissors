a725c18c4b3fada21d5b2dd8920ddab7849f0b89
Naol <37399395+Belayne@users.noreply.github.com>
2023-06-07 18:52:20 +0200

    Add function to play one round

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 
 const MOVES = ["ROCK", "PAPER", "SCISSORS"];
-const inputMessage = "Rock! Paper! Scissors!\n\nType your choice."
-const invalidInput = "Try again\n\nYou can only input rock, paper or scissors."
+const INPUT_MESSAGE = "Rock! Paper! Scissors!\n\nType your choice."
+const INVALID_INPUT = "Try again\n\nYou can only input rock, paper or scissors."
 
 function getComputerChoice() {
     let computerChoice = MOVES[Math.floor(Math.random() * 3)];
@@ -9,11 +9,11 @@ function getComputerChoice() {
 }
 
 function getUserChoice() {
-    let userChoice = prompt(inputMessage).toUpperCase();
+    let userChoice = prompt(INPUT_MESSAGE).toUpperCase();
 
     while(!checkInput(userChoice)) {
-        alert(invalidInput);
-        userChoice = prompt(inputMessage).toUpperCase();
+        alert(INVALID_INPUT);
+        userChoice = prompt(INPUT_MESSAGE).toUpperCase();
     }
     return userChoice;
 }
@@ -23,4 +23,35 @@ function checkInput(userChoice) {
     return valid;
 }
 
-console.log(getUserChoice());
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `Draw! You both put ${playerSelection}.`;
+    }
+
+    switch(playerSelection) {
+        case "ROCK": {
+                if(computerSelection === "SCISSORS") {
+                    return `You Win! ${playerSelection} beats ${computerSelection}.`
+                }
+                else return `You Lose! ${computerSelection} beats ${playerSelection}.`
+        }
+        case "SCISSORS": {
+            if(computerSelection === "PAPER") {
+                return `You Win! ${playerSelection} beats ${computerSelection}.`
+            }
+            else return `You Lose! ${computerSelection} beats ${playerSelection}.`
+        }
+        case "PAPER": {
+            if(computerSelection === "ROCK") {
+                return `You Win! ${playerSelection} beats ${computerSelection}.`
+            }
+            else return `You Lose! ${computerSelection} beats ${playerSelection}.`
+        }
+    }
+}
+
+let playerSelection = getUserChoice();
+let computerSelection = getComputerChoice();
+
+
+console.log(playRound(playerSelection, computerSelection))
