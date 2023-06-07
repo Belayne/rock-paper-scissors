bcfb1de3b7d2b90577160161839bc1ff75971e4a
Naol <37399395+Belayne@users.noreply.github.com>
2023-06-07 17:56:24 +0200

    Add computer choice

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,7 @@
-console.log("Test")
+
+const MOVES = ["ROCK", "PAPER", "SCISSORS"];
+
+function getComputerChoice() {
+    let computerMove = MOVES[Math.floor(Math.random() * 3)];
+    return computerMove;
+}
