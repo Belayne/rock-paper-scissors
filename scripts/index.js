59555c192b237735956301001e5c87885d655b7e
Naol <37399395+Belayne@users.noreply.github.com>
2023-06-07 18:18:36 +0200

    Add check for user input

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 
 const MOVES = ["ROCK", "PAPER", "SCISSORS"];
-const inputMessage = "Rock! Paper! Scissors!\nType your choice"
+const inputMessage = "Rock! Paper! Scissors!\n\nType your choice."
+const invalidInput = "Try again\n\nYou can only input rock, paper or scissors."
 
 function getComputerChoice() {
     let computerChoice = MOVES[Math.floor(Math.random() * 3)];
@@ -9,7 +10,17 @@ function getComputerChoice() {
 
 function getUserChoice() {
     let userChoice = prompt(inputMessage).toUpperCase();
+
+    while(!checkInput(userChoice)) {
+        alert(invalidInput);
+        userChoice = prompt(inputMessage).toUpperCase();
+    }
     return userChoice;
+}
+
+function checkInput(userChoice) {
+    let valid = MOVES.includes(userChoice);
+    return valid;
 }
 
 console.log(getUserChoice());
