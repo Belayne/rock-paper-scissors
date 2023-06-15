ffdfda7c14f056bb3d43efc0304546685930e85e
Naol <37399395+Belayne@users.noreply.github.com>
2023-06-15 09:40:12 +0200

    Add score and win condition

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,29 @@
+//TODO: Display the running score, and announce a winner of the game once one player reaches 5 points.
+
 
 const MOVES = ["ROCK", "PAPER", "SCISSORS"];
 
 const playerBtns = document.querySelectorAll('button');
 const resultDiv = document.querySelector('div.result-div');
+const playerScoreSpan = document.querySelector('p.player-score span');
+const computerScoreSpan = document.querySelector('p.computer-score span');
 
-playerBtns.forEach(
-    btn => btn.addEventListener("mousedown",playRound)
-    );
+let computerWins = 0;
+let playerWins = 0;
+
+startGame();
+
+function startGame() {
+    playerBtns.forEach(
+        btn => btn.addEventListener("mousedown",playRound)
+        );
+}
+
+function stopGame() {
+    playerBtns.forEach(
+        btn => btn.removeEventListener("mousedown",playRound)
+        );
+}
 
 function getComputerChoice() {
     let computerChoice = MOVES[Math.floor(Math.random() * 3)];
@@ -32,6 +49,34 @@ function drawDiv(text) {
     resultDiv.textContent = text;
 }
 
+function updateScore(result) {
+    if(result == 1) {
+        playerWins++;
+    }
+    else if(result == 2) {
+        computerWins++;
+    }
+    drawScore();
+    checkWin();
+}
+
+function drawScore() {
+    playerScoreSpan.textContent = `${playerWins}`;
+    computerScoreSpan.textContent = `${computerWins}`
+}
+
+function checkWin() {
+    if(playerWins == 5 || computerWins == 5) {
+        let text = (playerWins > computerWins)? "You Won! Congratulations.": "You Lost! Better luck next time."
+        drawDiv(text)
+        stopGame();
+    }
+}
+
+function drawWin(text) {
+
+}
+
 //Return
 //0: Draw
 //1: Win
@@ -39,51 +84,63 @@ function drawDiv(text) {
 function playRound(e) {
     let playerSelection = this.getAttribute('data-selection');
     let computerSelection = getComputerChoice();
+    let result;
 
     if (playerSelection === computerSelection) {
         let resultText = `Draw! You both put ${playerSelection}.`;
         drawDiv(resultText);
-        return 0;
+        result = 0;
     }
 
-    switch(playerSelection) {
-        case "ROCK": {
-            if(computerSelection === "SCISSORS") {
-                let resultText = `You Win! ${playerSelection} beats ${computerSelection}.`;
-                drawDiv(resultText);
-                return 1;
+    else {
+
+        switch(playerSelection) {
+            case "ROCK": {
+                if(computerSelection === "SCISSORS") {
+                    let resultText = `You Win! ${playerSelection} beats ${computerSelection}.`;
+                    drawDiv(resultText);
+                    result = 1;
+                    break;
+                }
+                else {
+                    let resultText = `You Lose! ${computerSelection} beats ${playerSelection}.`;
+                    drawDiv(resultText);
+                    result = 2;
+                    break;
+                }
             }
-            else {
-                let resultText = `You Lose! ${computerSelection} beats ${playerSelection}.`;
-                drawDiv(resultText);
-                return 2;
+            case "SCISSORS": {
+                if(computerSelection === "PAPER") {
+                    let resultText = `You Win! ${playerSelection} beats ${computerSelection}.`;
+                    drawDiv(resultText);
+                    result = 1;
+                    break;
+                }
+                else {
+                    let resultText = `You Lose! ${computerSelection} beats ${playerSelection}.`;
+                    drawDiv(resultText);
+                    result = 2;
+                    break;
+                }
             }
-        }
-        case "SCISSORS": {
-            if(computerSelection === "PAPER") {
-                let resultText = `You Win! ${playerSelection} beats ${computerSelection}.`;
-                drawDiv(resultText);
-                return 1;
-            }
-            else {
-                let resultText = `You Lose! ${computerSelection} beats ${playerSelection}.`;
-                drawDiv(resultText);
-                return 2;
-            }
-        }
-        case "PAPER": {
-            if(computerSelection === "ROCK") {
-                let resultText = `You Win! ${playerSelection} beats ${computerSelection}.`;
-                drawDiv(resultText);
-                return 1;
-            }
-            else {
-                let resultText = `You Lose! ${computerSelection} beats ${playerSelection}.`;
-                drawDiv(resultText);
-                return 2;
+            case "PAPER": {
+                if(computerSelection === "ROCK") {
+                    let resultText = `You Win! ${playerSelection} beats ${computerSelection}.`;
+                    drawDiv(resultText);
+                    result = 1;
+                    break;
+                }
+                else {
+                    let resultText = `You Lose! ${computerSelection} beats ${playerSelection}.`;
+                    drawDiv(resultText);
+                    result = 2;
+                    break;
+                }
             }
         }
     }
+
+    updateScore(result);
 }
 
 function game(numberOfGames) {
